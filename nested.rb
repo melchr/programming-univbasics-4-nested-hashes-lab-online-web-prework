a3215e8cd1598dcc06aa3867a18a7a18679f8827
Melissa Christman <melchrstmn@gmail.com>
2019-09-11 11:56:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -83,7 +82,12 @@ def adding_matz
         }
      }
 
-    
+
+
+  programmer_hash[:yukihiro_matsumoto] = {}
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  programmer_hash
 end
 
 def changing_alan
